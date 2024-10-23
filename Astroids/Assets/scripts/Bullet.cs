155dd3323f3f53550e78d3e80d4d36ab13ba5446
AshRapicault <aaliyah.rapicault@ll.sintgregorius.be>
2024-10-23 09:28:38 +0200

    bullet toegevoegd

---
+++ Astroids/Assets/scripts/Bullet.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+}
